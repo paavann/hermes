f8a218d35d5ebbb495fc2135a54a5e6f48fb99e3
paavann <pavann22826@gmail.com>
2026-09-03 17:44:41 +0530

    upgraded react-router to version 8.
    though there were some errors regarding the future key in react-router.config.ts. i'm gonna ignore it for now.

--- apps/hermes/react-router.config.ts
+++ apps/hermes/react-router.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  future: {
-    v8_middleware: true,
-    v8_splitRouteModules: true,
-    v8_passThroughRequests: true,
-    v8_trailingSlashAwareDataRequests: true,
-  },
+  // future: {
+  //   v8_middleware: true,
+  //   v8_splitRouteModules: true,
+  //   v8_passThroughRequests: true,
+  //   v8_trailingSlashAwareDataRequests: true,
+  // },
   ssr: true,
 } satisfies Config;
