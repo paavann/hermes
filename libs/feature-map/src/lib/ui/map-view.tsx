1052e604f109edadc0e6217f3216b544a39803c6
paavann <pavann22826@gmail.com>
2026-09-07 15:46:09 +0530

    the news are now being plotted on the map.

--- libs/feature-map/src/lib/ui/map-view.tsx
+++ libs/feature-map/src/lib/ui/map-view.tsx
@@ -1,13 +1,31 @@
 import { useEffect, useRef } from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
-
 import { useMapStore } from '../store/store'
 import { useMapDataSync } from '../hooks/datasync'
-
-
+import type { FeatureCollection } from 'geojson'
+import type { MapEventResponse } from '@hermes/util-types'
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN
+
+
+
+const createGeoJson = (events: MapEventResponse[]): FeatureCollection => {
+    return {
+        type: 'FeatureCollection',
+        features: events.map((event, index) => ({
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [event.longitude, event.latitude],
+            },
+            properties: {
+                ...event,
+                rank: index + 1
+            },
+        }))
+    }
+}
 
 
 
@@ -47,13 +65,84 @@ export function MapView() {
             }
         }
 
-        map.current.on('load', updateBounds)
+        map.current.on('load', () => {
+            updateBounds()
+            map.current?.addSource('events-source', {
+                type: 'geojson',
+                data: { type: 'FeatureCollection', features: [], },
+                cluster: true,
+                clusterMaxZoom: 14,
+                clusterRadius: 50,
+            })
+            map.current?.addLayer({
+                id: 'clusters',
+                type: 'circle',
+                source: 'events-source',
+                filter: ['has', 'point_count'],
+                paint: {
+                    'circle-color': "#475569",
+                    'circle-radius': [
+                        'step',
+                        ['get', 'point_count'],
+                        20,
+                        10,
+                        30,
+                        50,
+                        40,                     
+                    ],
+                    'circle-stroke-width': 2,
+                    'circle-stroke-color': "#94a3b8",
+                }
+            })
+            map.current?.addLayer({
+                id: 'cluster-count',
+                type: 'symbol',
+                source: 'events-source',
+                filter: ['has', 'point_count'],
+                layout: {
+                    'text-field': '{point_count_abbreviated}',
+                    'text-font': ['Arial Unicode MS Bold'],
+                    'text-size': 12,
+                },
+                paint: {
+                    'text-color': '#ffffff',
+                }
+            })
+            map.current?.addLayer({
+                id: 'unclustered-point',
+                type: 'circle',
+                source: 'events-source',
+                filter: ['!', ['has', 'point_count']],
+                paint: {
+                    'circle-color': ['get', 'category_color'],
+                    'circle-radius': [
+                        'match',
+                        ['get', 'rank'],
+                        1, 18,
+                        2, 14,
+                        3, 10,
+                        6
+                    ],
+                    'circle-stroke-width': 1,
+                    'circle-stroke-color': "#000000",
+                }
+            })
+        })
         map.current.on('moveend', updateBounds)
         return () => {
             map.current?.remove()
             map.current = null
         }
     }, [setViewport])
+
+    useEffect(() => {
+        if(!map.current || !map.current.isStyleLoaded()) return
+        const source = map.current.getSource('events-source') as mapboxgl.GeoJSONSource
+        if(source && events) {
+            const geojsonData = createGeoJson(events)
+            source.setData(geojsonData)
+        }
+    }, [events])
 
     
     return (
